5ad5f1a3a48dfb20c4d4450e0eada738c31c7794
druwa <wbsl0427@gmail.com>
2021-08-31 02:38:18 +0900

    add application

---
+++ gannamstyle/android/app/src/main/kotlin/com/example/gannamstyle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gannamstyle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gannamstyle/lib/cameraview.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:gannamstyle/picture.dart';
+
+// 사용자가 주어진 카메라를 사용하여 사진을 찍을 수 있는 화면
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // 카메라의 현재 출력물을 보여주기 위해 CameraController를 생성합니다.
+    _controller = CameraController(
+      // 이용 가능한 카메라 목록에서 특정 카메라를 가져옵니다.
+      widget.camera,
+      // 적용할 해상도를 지정합니다.
+      ResolutionPreset.ultraHigh,
+      imageFormatGroup: ImageFormatGroup.yuv420,
+    );
+
+    // 다음으로 controller를 초기화합니다. 초기화 메서드는 Future를 반환합니다.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // 위젯의 생명주기 종료시 컨트롤러 역시 해제시켜줍니다.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      // 카메라 프리뷰를 보여주기 전에 컨트롤러 초기화를 기다려야 합니다. 컨트롤러 초기화가
+      // 완료될 때까지 FutureBuilder를 사용하여 로딩 스피너를 보여주세요.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            //_controller.startImageStream(_processCameraImage);
+            // Future가 완료되면, 프리뷰를 보여줍니다.
+            return Container(
+              color: Colors.black,
+              child: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  CameraPreview(_controller),
+                ],
+              ),
+            );
+          } else {
+            // 그렇지 않다면, 진행 표시기를 보여줍니다.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // onPressed 콜백을 제공합니다.
+        onPressed: () async {
+          // try / catch 블럭에서 사진을 촬영합니다. 만약 뭔가 잘못된다면 에러에
+          // 대응할 수 있습니다.
+          try {
+            // 카메라 초기화가 완료됐는지 확인합니다.
+            await _initializeControllerFuture;
+
+            final image = await _controller.takePicture();
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: image.path),
+              ),
+            );
+          } catch (e) {
+            // 만약 에러가 발생하면, 콘솔에 에러 로그를 남깁니다.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
